4e02dff1d0adb4a4a3a7de89f3f69b05094832cb
HabebeZyad <habebazyad2114@gmail.com>
2026-02-27 03:35:25 +0200

    Fix GitHub pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // Add your GitHub repository name here
+  base: "/-Comes-To-Life-/",
+
   server: {
     host: "::",
     port: 8080,
